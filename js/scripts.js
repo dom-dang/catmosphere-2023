846396ca0373e8710f3d45195ac09329441f88b9
Dom <57075586+dom-dang@users.noreply.github.com>
2023-09-17 10:57:09 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var playing = false;
+
+function music() {
+  var song = document.getElementById("music");
+  var photo = document.getElementById("sound-photo");
+  if (playing === false) {
+    song.play();
+    photo.src = "assets/low-volume.png";
+    playing = true;
+  } else {
+    song.pause();
+    photo.src= "assets/muted.png";
+    playing = false;
+  }
+  
+}
+
+
+
+
+
+var myVar;
+
+function loading() {
+  document.body.style.backgroundImage = "url('css/loading-pic.jpg')";
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.body.style.backgroundImage = "url('css/home-screen.jpg')";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
